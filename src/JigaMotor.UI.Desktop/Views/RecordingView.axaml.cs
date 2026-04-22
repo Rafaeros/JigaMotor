61f567aedd64af858fa76944d35decda99efbda1
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-04-22 15:42:47 -0300

    [feat] initial static desktop ui

---
+++ src/JigaMotor.UI.Desktop/Views/RecordingView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace JigaMotor.UI.Desktop.Views
+{
+    public partial class RecordingView : UserControl
+    {
+        public RecordingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/JigaMotor.UI.Desktop/Views/SettingsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace JigaMotor.UI.Desktop.Views
+{
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
